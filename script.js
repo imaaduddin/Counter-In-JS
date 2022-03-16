25ace19eafe6915b038bffe374083edf79d07f9c
Imaad Uddin <imaaduddin27@gmail.com>
2022-03-15 19:22:05 -0500

    styling and some js

---
+++ script.js
@@ -0,0 +1,12 @@
+// Buttons and counter text
+const counter = document.querySelector('#counter');
+const btns = document.querySelector('.btn');
+
+// Initialize the count variable
+let count = 0;
+
+btns.foreach((btn) => {
+    btn.addEventListener('click', (e) => {
+        const styles = e.currentTarget.classList;
+    })
+})
